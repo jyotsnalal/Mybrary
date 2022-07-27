d768eaa6483e4edc84a6b4cb97d0fe65f72688b1
Jyotsnalal <das.jyotsna13@gmail.com>
2022-07-27 12:29:49 +0530

    make it better

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
-
+const authorRouter = require("./routes/authors");
 const mongoose = require("mongoose");
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
@@ -20,5 +21,6 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
