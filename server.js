6f8953599cec3fe4b187e22e2d9247bba27496a0
Jyotsnalal <das.jyotsna13@gmail.com>
2022-07-28 20:55:09 +0530

    Inital author routes

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ if (process.env.NDE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
-const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("connected to Mongoose"));
 //set view engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -20,6 +17,14 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to Mongoose"));
+
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 
